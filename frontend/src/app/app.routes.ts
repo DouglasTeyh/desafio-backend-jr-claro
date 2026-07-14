661ad30ec829979cd2edbceb827e616eda9c769e
Douglas Teyh <contato.douglasteyh@gmail.com>
2026-07-14 20:47:14 -0300

    feat(ui): base routing setup

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ListagemComponent } from './pages/listagem/listagem.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'listagem', component: ListagemComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: '**', redirectTo: 'login' }
+];
